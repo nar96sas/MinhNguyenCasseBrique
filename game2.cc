c5aa791b08025125a6183deb9e26fde376f06372
nar96sas <chungminhnguyen12@gmail.com>
2015-02-11 16:31:43 +0700

    Revert "new version"
    This reverts commit 11289fb70be9ebaae91b0c9b2662c37f7935b093.

--- game2.cc
+++ game2.cc
@@ -11,85 +11,52 @@ using namespace std;
  * sinon retourne le code ASCII de la touche */
 int read_keybord();
 
-/***************************************************************************/
-#define H  20
-#define L  50
+/******************************************************************************/
+#define H 20
+#define L 30
 
 char screen[H][L];
 
 void screen_display() {
   system("clear");
   for (int i=0; i<H; i++) {
-    for (int j=0; j<L; j++)
+    for (int j=0; j<L; j++) {
       cout << screen[i][j];
-    cout << endl;
-  }
-}
-
-void display() {
-  for (int i=0; i<H; i++){
-    for (int j=0; j<L; j++){
-      if (i==0 || i==H-1 || j==0 || j==L-1)
-	screen[i][j] = '*';
-      else
-	screen[i][j] = ' ';
     }
     cout << endl;
   }
 }
-/******************************************************************************/
-double x = H-2;
-double y = L/2;
-bool movex=true; 
-bool movey=true;
 
-void update_game(int key) {
-  screen[(int)x][(int)y] = 'o';
-  if (movex){
-    x--; 
-    if (x == 0)
-      movex=false;
-  }
-  
-  if (!movex) {
-    x++;
-    if (x == H-2)
-      movex=true;
-  }
-  
-  if (movey){
-    y++; 
-    if (y == L-2)
-      movey=false;
-  }
-
-  if (!movey) {
-    y--;
-    if (y == 1)
-      movey=true;
-  }  
+void clear_screen() {
+  for (int i=0; i<H; i++)
+    for (int j=0; j<L; j++)
+      screen[i][j] = ' ';
 }
 
-  
+/******************************************************************************/
+double x = 0.0;
+double y = 0.0;
 
-
-
+void update_game(int key) {
+  screen[(int)x][(int)y] = 'X';
+  x = x + 0.2;
+  y = y + 0.05;
+}
 
 /******************************************************************************/
+
 int main() {
   int key;
   do {
     key = read_keybord();
-    display();
+    clear_screen();
     update_game(key);
     screen_display();
-    usleep(100 * 1000);
-  } 
-  while (key != 'q');
+    usleep(20 * 1000);
+  } while (key != 'q');
 }
 
 /******************************************************************************/
-
 
 int kbhit(void)
 {
@@ -125,7 +92,7 @@ int read_keybord() {
   else
     key = 0;
   return key;
-}                     
+}
 
 /******************************************************************************/
 /******************************************************************************/
